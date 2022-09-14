8b9999a4fcba863d62602d1ffc6c8260a4602ee2
ckmishra2 <chandrakalatiwari2@gmail.com>
2022-09-14 20:41:31 +0530

    Add: use generic method for string and float

--- GenericClass.java
+++ GenericClass.java
@@ -18,6 +18,10 @@ public class GenericClass {
 		
 			  public static void main(String args[]) {
 			    System.out.printf("Maximum of %d, %d and %d is %d\n\n", 70, 80, 50, maximum(70, 80, 50));
+			    System.out.printf("Maximum of %.1f, %.1f and %.1f is %.1f\n\n", 6.6, 8.8, 7.7, maximum(6.6,
+			        8.8, 7.7));
+			    System.out.printf("Maximum of %s, %s and %s is %s\n", "pear", "apple", "orange", maximum(
+			        "pear", "apple", "orange"));
 			  
 			  }
 		}
