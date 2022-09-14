3cd59f66feaf6fd051553bd08a0da59cc821bb2d
ckmishra2 <chandrakalatiwari2@gmail.com>
2022-09-14 20:24:26 +0530

    Modified:find maximum among three

--- GenericClass.java
+++ GenericClass.java
@@ -2,25 +2,22 @@ package generic.genericmethod;
 
 public class GenericClass {
 
-	public static void main(String[] args) {
-		System.out.println("program to find maximum number among three numbers");
+
 	// Three Integer objects initialization
-		Integer firstNumber = 200 ;
-		Integer secondNumber = 100 ;
-		Integer thirdNumber = 150 ;
-
-		Integer max = firstNumber ;
-
-		// using compareTo method
-		if(secondNumber.compareTo(max) > 0) {
-			max = secondNumber;
+		 public static <T extends Comparable<T>> T maximum(T x, T y, T z) {
+			    T max = x; // assume x is initially the largest
+		
+			    if (y.compareTo(max) > 0)
+			      max = y; // y is the largest so far
+		
+			    if (z.compareTo(max) > 0)
+			      max = z; // z is the largest
+		
+			    return max; // returns the largest object
+			  } // end method maximum
+		
+			  public static void main(String args[]) {
+			    System.out.printf("Maximum of %d, %d and %d is %d\n\n", 70, 60, 50, maximum(70, 60, 50));
+			  
+			  }
 		}
-		if(thirdNumber.compareTo(max) > 0) {
-			max = thirdNumber ;
-		}
-
-		// printing the maximum among three integer objects
-		System.out.println("Maximum number is "+max);
-	}
-
-}
